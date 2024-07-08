8b75a6f515b178ddc023cbabe0f62320fe873e92
Avin Varghese Mathew <avinvmavolil@gmail.com>
2024-07-08 16:34:39 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+console.log("Welcome");
+
+let value = 6;
+let second = value + 5;
+{
+    let value = 7;
+    console.log("Inside value :",value)
+}
+// let output = value + second;
+console.log("Outside value :" ,value);
